282bba2d50811eb806541dd32fdf73a607566210
diegoib <diego.ibayo@gmail.com>
2022-08-19 09:41:40 +0200

    updated ingestion, training and scoring

--- ingestion.py
+++ ingestion.py
@@ -17,10 +17,20 @@ output_folder_path = config['output_folder_path']
 
 
 #############Function for data ingestion
-def merge_multiple_dataframe():
+def merge_multiple_dataframe(input_folder_path, output_folder_path):
     #check for datasets, compile them together, and write to an output file
+    csv_files = os.listdir(input_folder_path)
+    final_df = pd.DataFrame()
+    for f in csv_files:
+        new_df = pd.read_csv(f)
+        final_df = pd.concat([final_df, new_df], axis=0)
+    final_df = final_df.drop_duplicates()
+    final_df.to_csv(os.path.join(output_folder_path, 'finaldata.csv'), index=False)
+   
+    with open(os.path.join(output_folder_path, 'ingestedfiles.txt'), 'wb') as file:
+        file.write(csv_files)
 
 
 
 if __name__ == '__main__':
-    merge_multiple_dataframe()
+    merge_multiple_dataframe(input_folder_path, output_folder_path)
--- scoring.py
+++ scoring.py
@@ -15,11 +15,20 @@ with open('config.json','r') as f:
     config = json.load(f) 
 
 dataset_csv_path = os.path.join(config['output_folder_path']) 
-test_data_path = os.path.join(config['test_data_path']) 
+model_path = os.path.join(config['output_model_path'])
+test_data_path = os.path.join(config['test_data_path'])
 
 
 #################Function for model scoring
-def score_model():
+def score_model(test_data_path, model_path):
     #this function should take a trained model, load test data, and calculate an F1 score for the model relative to the test data
     #it should write the result to the latestscore.txt file
-
+    df = pd.read_csv(os.path.join(test_data_path, 'testdata.csv'))
+    with open(os.path.join(model_path, 'trainedmodel.pkl'), 'rb') as file:
+        model = pickle.load(file)
+    X = df[['lastmonth_activity', '', '']].copy()
+    y = df['exited'].copy()
+    predictions = model.predict(X)
+    f1score = metrics.f1_score(y, predictions)
+    with open(os.path.join(model_path, 'latestscore.txt')) as file:
+        file.write(f1score)
